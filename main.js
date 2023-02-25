51635c8332b46f2613bb76b7e251ad59f1593bea
Taras Slovak <slovak.taras@gmail.com>
2023-02-25 23:52:56 +0200

    finished part 1

---
+++ main.js
@@ -0,0 +1,9 @@
+import './public/styles/reset.css';
+import './public/styles/fonts.css';
+import './style.css';
+import './public/styles/header.css';
+import './public/styles/promo.css';
+import './public/styles/features.css';
+import './public/styles/cards.css';
+import './public/styles/mission.css';
+// import './public/scripts/counter.js';
